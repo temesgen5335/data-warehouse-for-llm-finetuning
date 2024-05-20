0807b71ab14af92880843382fc7fa84879570b5f
Hillary Kipkemoi <tenskap@gmail.com>
2024-05-20 13:54:38 +0300

    Intial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+@app.get("/hello/user_{name}/{user_id}")
+async def say_hello(user_id: int, name: str):
+    response = {
+        "message": f"Hello {name}",
+        "user_id": f"{user_id}"
+    }
+    return response
