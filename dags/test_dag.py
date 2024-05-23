b2bd9bcb67419288e8719924b121c8c46773d366
Hillary Kipkemoi <tenskap@gmail.com>
2024-05-23 15:12:52 +0300

    create dag for datawarehouse

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -4,14 +4,13 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime
 
 with DAG(
-    dag_id='llm_data_warehouse',
+    dag_id='test_hello_world',
     start_date=datetime(2024, 5, 23),
     schedule_interval='@daily',
     tags=['llm', 'dw', 'hello_world'],
 ) as dag:
     # Define task to test hello world
-    data_collection = BashOperator(
+    task1 = BashOperator(
         task_id='task1',
         bash_command='echo "Hello World"',
     )
-
