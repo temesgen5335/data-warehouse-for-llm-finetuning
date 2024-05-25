c121111818ee21a82eb25ead2c1339467f141f96
temesgen5335 <temesgen5335@gmail.com>
2024-05-25 10:45:50 +0300

    data cleaning

--- Web_scrapping/beautiful.py
+++ Web_scrapping/beautiful.py
@@ -1,25 +1,40 @@
-from selenium import webdriver
+#from selenium import webdriver
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import pandas as pd
+import requests
 import time
+import re
 
-# Set up Selenium webdriver
-driver = webdriver.Firefox()
-url = "https://www.ethiobookreview.com/book/ye-ethiopia-tarik-ke-lusi-arat-million"
-driver.get(url)
-time.sleep(3)  # Let the page load
 
-# Use BeautifulSoup to parse the page source
-soup = BeautifulSoup(driver.page_source, 'html.parser')
+url ="https://am.wikipedia.org/wiki/%E1%8B%A8%E1%8A%A2%E1%89%B5%E1%8B%AE%E1%8C%B5%E1%8B%AB_%E1%8A%90%E1%8C%88%E1%88%A5%E1%89%B3%E1%89%B5"
 
-# Find the element containing the text data
-text_data_element = soup.find('div', class_='et_pb_module et_pb_text et_pb_text_1 et_pb_bg_layout_light')
 
-if text_data_element:
-    # Extract text data
-    text_data = text_data_element.get_text()
-    print(text_data)
-else:
-    print("Text data element not found")
+html = urlopen(url)
 
-# Close the Selenium webdriver
-driver.quit()
+
+# Fetch the HTML content
+response = requests.get(url)
+html_content = response.content
+
+soup = BeautifulSoup(html_content, 'html.parser')
+
+# Extract text from paragraphs (adjust selector as needed)
+
+texts = soup.find_all("p")
+list_texts = []
+
+for text in texts:
+    lines = text.get_text(strip=True)
+    str_lines = str(lines)
+    clean = re.compile('<.*==?>') #compiles a regular expression pattern
+    clean2 = (re.sub(clean, '' ,str_lines)) # used the re.sub() method to replace any matches of this pattern in str_cells with an empty string
+    list_texts.append(clean2)
+
+print(list_texts)
+
+#text_df = pd.DataFrame(list_texts)
+#df1 = text_df[0].str.split('·', expand=True)
+#df1[0] = df1[0].str.strip('[')
+#df1#
+
