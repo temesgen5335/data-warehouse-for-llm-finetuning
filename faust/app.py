dcd8e2789a53a2d7b6609c17a953aac219745ef9
Hillary Kipkemoi <tenskap@gmail.com>
2024-05-27 13:01:07 +0300

    add comment for description

--- faust/app.py
+++ faust/app.py
@@ -19,10 +19,14 @@ mongodb = MongoDB('bbc_news', 'scraped_data', MONGODB_CONNECTION_STRING)
 @app.agent(scraped_data_topic)
 async def process_data(scraped_data):
   async for data in scraped_data:
+    # HERE WE PREPROCESS THE DATA
+    # For now we'll just convert the title to uppercase
+
     # convert the title to uppercase
     data.url = data.url.upper()
     list_data = [data.to_dict()]
 
+    # Save the data to MongoDB
     mongodb.insert_content(data.to_dict())
     # print(type(data))
 
