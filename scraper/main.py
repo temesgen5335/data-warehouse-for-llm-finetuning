56e45a9244e34abd43ad3178c9936c7b56b8b1d5
hillaryke <tenskap@gmail.com>
2024-05-28 14:08:46 +0000

    template for scraping news

--- scraper/main.py
+++ scraper/main.py
@@ -1,16 +1,24 @@
-from news_scrapper import NewsScraper
+from news_scrapper import NewsScraper, NewsCategory
+import pprint
 
-def main():
+def hello():
     url = 'https://am.al-ain.com/'
     pages_to_scrape = 2
     try:
-        scraper = NewsScraper(url=url, number_of_pages_to_scrape=pages_to_scrape)
-        news_data = scraper.get_full_news()
+        print("Scraping started ")
 
-        # print(news_data)
-        print(news_data[0])
+        scraper = NewsScraper(url, pages_to_scrape)
+        # Initialize the scraper for a specific category
+        # scraper.initialize_driver(NewsCategory.POLITICS)
+        news = scraper.scrape_news()
+        for article in news:
+            pprint(article)
+
+        # Get the next article value
+        # article = sc.get_next_article_value()
+
     except Exception as e:
         print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    main()
+    hello()
