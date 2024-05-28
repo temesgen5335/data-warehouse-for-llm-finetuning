df2417da39d7af9bd8d569699638982fd5386464
hillaryke <tenskap@gmail.com>
2024-05-28 14:06:39 +0000

    faust process scraped news

--- faust/app.py
+++ faust/app.py
@@ -4,16 +4,17 @@ import os
 from database import MongoDB
 
 from datetime import datetime
-from models import ScrapedData
+from models import ScrapedNews
 
 KAFKA_BROKER_URL = os.getenv('KAFKA_BROKER_URL', 'kafka://localhost:9093')
 MONGODB_CONNECTION_STRING = 'mongodb://host.docker.internal:27017/'
 
 app = faust.App('myapp', broker=KAFKA_BROKER_URL)
-scraped_data_topic = app.topic('scraped_data', value_type=ScrapedData)
+scraped_data_topic = app.topic('scraped_news', value_type=ScrapedNews)
 
 # create a MongoDB instance (for saving the data after processing)
 mongodb = MongoDB('bbc_news', 'scraped_data', MONGODB_CONNECTION_STRING)
+
 
 @app.agent(scraped_data_topic)
 async def process_data(scraped_data):
@@ -22,14 +23,17 @@ async def process_data(scraped_data):
     # For now we'll just convert the title to uppercase
 
     # convert the title to uppercase
-    data.url = data.url.upper()
-    list_data = [data.to_dict()]
+    data.article_url = data.article_url.upper()
+    
+    data = data.asdict()
 
     # Save the data to MongoDB
-    mongodb.insert_content(data.to_dict())
-    # print(type(data))
+    # mongodb.insert_content(data.to_dict())
 
-    print(list_data)
+    print(type(data))
+    print(data.get("article_url"))
+    # print(data.article_url)
+
 
 if __name__ == '__main__':
     app.main()
