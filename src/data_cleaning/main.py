3d0a60146770756a4e239ba2aef5257c7e03057d
Hillary Kipkemoi <tenskap@gmail.com>
2024-05-29 09:34:51 +0300

    process all articles on mongodb collection

--- src/data_cleaning/main.py
+++ src/data_cleaning/main.py
@@ -67,11 +67,29 @@ def process_all_articles_in_collection():
     print("Processing all articles in the collection...")
 
     # Fetch all articles from the current database of unclean data
-    articles = scraped_bbc.get_all_content_as_list()
+    articles = scraped_bbc.find()
 
     # Process each article
     for article in articles:
-        process_article()
+        # Save the _id of the article before processing it
+        article_id = article['_id']
+
+        # Process the article
+        if article is not None:
+            # Process the article
+            clean_title = process_text_and_display(article.get('title'))
+
+            # Update the article with the cleaned title
+            article['title'] = clean_title
+
+            try:
+                # Try to save the processed article to the cleaned data database
+                cleaned_data_collection.insert_content(article)
+            except DuplicateKeyError:
+                print(f"Article with _id {article['_id']} already exists in the cleaned_data collection.")
+
+            # Delete the original article that has been processed from the scraped data database
+            scraped_bbc.delete_one({'_id': article_id})
 
     print("All articles have been processed.")
 
@@ -80,8 +98,8 @@ def main():
     text = "ጽኑ ህክምና የሚያስፈልጋቸው ሲሆኑ፤ ከዚህ ውስጥ ከ40 እስከ"
     clean_text = process_text_and_display(text)
 
-    # process_all_articles_in_collection()
-    process_article()
+    process_all_articles_in_collection()
+    # process_article()
 
     # save the cleaned data to the database
 
