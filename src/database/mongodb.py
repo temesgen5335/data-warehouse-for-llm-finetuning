6a973f36bc670d50f2a85972f27c62e5cef71166
Hillary Kipkemoi <tenskap@gmail.com>
2024-05-29 08:13:50 +0300

    use mongomock in mongo tests

--- src/database/mongodb.py
+++ src/database/mongodb.py
@@ -3,17 +3,16 @@ from dotenv import load_dotenv
 from pymongo import MongoClient
 
 class MongoDB:
-    def __init__(self, collection_name: str = None, db_name: str = None, connection_string: str = None):
+    def __init__(self, collection_name: str = None, db_name: str = None, connection_string: str = None, client=None):
         load_dotenv()
         # TODO Add error handling for missing environment variables and arguments
         # provide the mongodb atlas url to connect python to mongodb using pymongo
         self.CONNECTION_STRING = os.getenv("MONGO_CONNECTION_STRING") if connection_string is None else connection_string
         self.MONGO_DB_NAME = os.getenv("MONGO_DB_NAME") if db_name is None else db_name
-        
         self.MONGO_COLLECTION_NAME = os.getenv("MONGO_COLLECTION_NAME") if collection_name is None else collection_name
 
         # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-        self.client = MongoClient(self.CONNECTION_STRING)
+        self.client = MongoClient(self.CONNECTION_STRING) if client is None else client
         self.db = self.client[self.MONGO_DB_NAME]
         self.collection = self.db[self.MONGO_COLLECTION_NAME]
 
