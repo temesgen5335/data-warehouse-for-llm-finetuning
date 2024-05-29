a47a1afca63c98b969b6d19955cc1b30351924a0
Hillary Kipkemoi <tenskap@gmail.com>
2024-05-29 10:42:22 +0300

    modularize the procesing steps

--- src/data_cleaning/main.py
+++ src/data_cleaning/main.py
@@ -10,99 +10,21 @@ from pymongo.errors import DuplicateKeyError
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from database.mongodb import MongoDB
-from data_cleaning_utils import Normalize
+from article_processor import ArticleProcessor
+
+
 
 archive_db = MongoDB(db_name='archive', collection_name='archive_data')
-cleaned_data_collection = MongoDB(db_name='clean_data', collection_name='cleaned_data')
-scraped_bbc = MongoDB(db_name='scraped_data', collection_name='bbc_news')
-
-
-def process_text_and_display(text):
-    # create an instance of the Normalize class
-    normalizer = Normalize()
-
-    # Test the function
-    print("\nOriginal text: ")
-    print(text)
-
-    preprocessed_text = normalizer.clean_data(text)
-    print("\n Preprocessed text:")
-    print(preprocessed_text, "\n")
-
-    print("==================Success==========================")
-
-
-    return preprocessed_text
-
-
-
-
-def process_article():
-    print("Processing article...")
-
-    # Fetch one article from the current database of unclean data
-    article = scraped_bbc.find_one()
-
-    # Save the _id of the article before processing it
-    article_id = article['_id']
-
-    # Process the article
-    if article is not None:
-        # Process the article
-        clean_title = process_text_and_display(article.get('title'))
-
-        # Update the article with the cleaned title
-        article['title'] = clean_title
-
-        try:
-            # Try to save the processed article to the cleaned data database
-            cleaned_data_collection.insert_content(article)
-        except DuplicateKeyError:
-            print(f"Article with _id {article['_id']} already exists in the cleaned_data collection.")
-
-        # Delete the original article that has been processed from the scraped data database
-        scraped_bbc.delete_one({'_id': article_id})
-
-def process_all_articles_in_collection():
-    print("Processing all articles in the collection...")
-
-    # Fetch all articles from the current database of unclean data
-    articles = scraped_bbc.find()
-
-    # Process each article
-    for article in articles:
-        # Save the _id of the article before processing it
-        article_id = article['_id']
-
-        # Process the article
-        if article is not None:
-            # Process the article
-            clean_title = process_text_and_display(article.get('title'))
-
-            # Update the article with the cleaned title
-            article['title'] = clean_title
-
-            try:
-                # Try to save the processed article to the cleaned data database
-                cleaned_data_collection.insert_content(article)
-            except DuplicateKeyError:
-                print(f"Article with _id {article['_id']} already exists in the cleaned_data collection.")
-
-            # Delete the original article that has been processed from the scraped data database
-            scraped_bbc.delete_one({'_id': article_id})
-
-    print("All articles have been processed.")
+cleaned_data_collection = MongoDB(db_name='clean_data', collection_name='slack_cleaned_data')
+slack_original_data = MongoDB(db_name='slack_data', collection_name='amharic_news_data')
 
 
 def main():
-    text = "ጽኑ ህክምና የሚያስፈልጋቸው ሲሆኑ፤ ከዚህ ውስጥ ከ40 እስከ"
-    clean_text = process_text_and_display(text)
+    text = "ጽኑ ህክምና የሚያስፈልጋ ቸው ሲሆኑ፤ ከዚህ ውስጥ ከ40 እስከ"
+    columns_to_process = ['title']  # Add other columns to process here
 
-    process_all_articles_in_collection()
-    # process_article()
-
-    # save the cleaned data to the database
-
+    processor = ArticleProcessor(slack_original_data, cleaned_data_collection, archive_db, columns_to_process)
+    processor.process_all_articles_in_collection()
 
 if __name__ == '__main__':
     main()
