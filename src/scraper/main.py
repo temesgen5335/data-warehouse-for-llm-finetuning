e27521447bce1b8f34611712f082da3781496061
Hillary Kipkemoi <tenskap@gmail.com>
2024-05-30 07:22:08 +0300

    feat: Update pages_to_scrape to a large number in main.py

--- src/scraper/main.py
+++ src/scraper/main.py
@@ -5,7 +5,7 @@ from kafka.errors import NoBrokersAvailable
 
 def hello():
     url = 'https://am.al-ain.com/'
-    pages_to_scrape = 3
+    pages_to_scrape = 1000000
     scraper = NewsScraper(url=url, number_of_pages_to_scrape=pages_to_scrape)
     scraper.scrape_news()
         # pages_to_scrape += 1
